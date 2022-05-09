97b7de77303fce8071d8688b8954e0f3a5aec259
khanumair1650 <khanumair1650@gmail.com>
2022-05-09 22:31:16 +0530

    final commit

--- api/HospitalRoutes.js
+++ api/HospitalRoutes.js
@@ -68,7 +68,7 @@ router.get('/search/:name',async(req,res)=>{
         if(hospital){
             res.status(200).json(hospital)
         }else{
-            res.status(401).json({error: "Hospital Not Found"})
+            res.status(404).json({error: "Hospital Not Found"})
         }
     }).catch(err =>{
         res.status(500).json({error:err.message})
